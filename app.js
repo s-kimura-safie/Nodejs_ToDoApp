969d46c394113b8b46ee4475278de1bb596a6122
s-kimura <s-kimura@safie.jp>
2024-11-07 22:07:05 +0900

    Change var name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static('./public')); // Serve static files
 
 require('dotenv').config(); // Import dotenv
 
-const taskRoutes = require('./routes/tasks'); // Import tasks.js
+const taskRoutes = require('./routes/taskRoutes'); // Import tasks.js
 const connectDB = require('./db/connect'); // Import connect.js
 
 const PORT = 5000;
@@ -20,8 +20,8 @@ app.use("/api/v1/tasks", taskRoutes); // Use /api/v1/tasks as a prefix for all r
 // データベース接続
 const start = async () => { // 非同期処理を行う関数を定義
     try {
-        await connectDB(process.env.MONGO_URL); // Connect to the database
-        app.listen(PORT, console.log("Server is running on port", PORT)); // If connected, start the server
+        await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL); // Connect to the database
+        app.listen(process.env.PORT || PORT , console.log("Server is running")); // If connected, start the server
     } catch (error) {
         console.log(error);
     }
